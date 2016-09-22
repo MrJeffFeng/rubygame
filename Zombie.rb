b7c380e83b8629ecf83c2a55633db6b2a9d0c953
Jeff Feng <MrJeffFeng@users.noreply.github.com>
2016-09-22 09:45:48 -0700

    Update Zombie.rb

--- Zombie.rb
+++ Zombie.rb
@@ -1,1 +1,19 @@
-print "hello World"
+class Zombie
+  
+  def initialize(x, y)
+    @real_x = x
+    @real_y = y
+    @stand_right = Image.load_tiles($self, "media/Zombie/zombie1.png", w, h, false)
+    @stand_left =
+    @walking_right = 
+    @walking_left = 
+  end
+  
+  def update
+    
+  end
+  
+  def draw
+  end
+  
+end
