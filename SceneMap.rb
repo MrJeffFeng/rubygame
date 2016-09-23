22409bcfae0a353634b150d2a033d5fdf69b3909
MrJeffFeng <jefffeng321@gmail.com>
2016-09-22 21:30:05 -0700

    Fixes

--- SceneMap.rb
+++ SceneMap.rb
@@ -3,14 +3,14 @@ class SceneMap
   def initialize
     @character = Character.new(400, 350)
   end
-  
+
   def update
-    @character.update 
+    @character.update
     @character.move_left if button_down? KbA
     @character.move_right if button_down? KbD
   end
-  
+
   def draw
     @character.draw
   end
-  end
+end
