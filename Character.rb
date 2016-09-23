1778e1207171ff362ae376e218186125ae30cec5
MrJeffFeng <jefffeng321@gmail.com>
2016-09-22 21:41:51 -0700

    Cleaning up

--- Character.rb
+++ Character.rb
@@ -1,14 +1,14 @@
 class Character
-  
+
   def initialize(x, y)
     @real_x = x
     @real_y = y
-    @stand_right = Image.load_tiles($window, "media/Character/character1.png", 96, 120, false)
-    @stand_left = Image.load_tiles($window, "media/Character/character1_left.png", 96, 120, false)
-    @walk_right = Image.load_tiles($window, "media/Character/character3.png", 106, 116, false)
-    @walk_left = Image.load_tiles($window, "media/Character/character3_left.png", 106, 116, false)
-    @shoot_right = Image.load_tiles($window, "media/Character/character_shooting3.png", 105, 117, false)
-    @shoot_left = Image.load_tiles($window, "media/Character/character_shooting3_left.png", 105, 117, false)
+    @stand_right = Image.load_tiles($window, "media/Character/stand_right.png", 96, 120, false)
+    @stand_left = Image.load_tiles($window, "media/Character/stand_left.png", 96, 120, false)
+    @walk_right = Image.load_tiles($window, "media/Character/walk_right.png", 106, 116, false)
+    @walk_left = Image.load_tiles($window, "media/Character/walk_left.png", 106, 116, false)
+    @shoot_right = Image.load_tiles($window, "media/Character/shooting_right.png", 105, 117, false)
+    @shoot_left = Image.load_tiles($window, "media/Character/shooting_left.png", 105, 117, false)
     @sprite = @stand_right
     @dir = :right
     @x = @real_x + (@sprite[0].width / 2)
@@ -17,11 +17,11 @@ class Character
     @moving = false
     @is_visible = true
   end
-  
+
   def update
     @real_x = @x - (@sprite[0].width / 2)
     @real_y = @y - @sprite[0].height
-    
+
     if @moving then
       if @move_x > 0 then
         @move_x -= 1
@@ -32,29 +32,30 @@ class Character
       elsif @move_x == 0 then
         @moving = false
       end
-    else   
+    else
       if @dir == :left then
         @sprite = @stand_left
       elsif @dir == :right then
         @sprite = @stand_right
       end
-    end 
+    end
     if button_down? KbSpace and @dir == :right
       @sprite = @shooting_right
     end
     if button_down? KbSpace and @dir == :left
       @sprite = @shooting_left
     end
-end
+  end
+
   def hide()  @is_visible = false     end
   def show()  @is_visible = true      end
-    
-def move_left
-    @dir = :left
-    @move_x = -5
-    @moving = true
-    @sprite = @walk_left
-end
+
+  def move_left
+      @dir = :left
+      @move_x = -5
+      @moving = true
+      @sprite = @walk_left
+  end
 
   def move_right
     @dir = :right
@@ -74,10 +75,10 @@ end
   def dir
     return @dir
   end
-  
+
   def draw(z=5) # Creates smooth costume switch
    frame = milliseconds / 150 % @sprite.size
    @sprite[frame].draw(@real_x, @real_y, z) if @is_visible
   end
-  
+
 end
