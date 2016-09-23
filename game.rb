4cf705de3818a8fe6fe1af8e49ab3c82d1eec620
MrJeffFeng <jefffeng321@gmail.com>
2016-09-23 08:22:51 -0700

    Simpler Code

--- game.rb
+++ game.rb
@@ -1,9 +1,6 @@
 require 'Gosu'
 require 'Sprite'
 require 'Background'
-require 'Character'
-require 'Zombie'
-require 'SceneMap'
 
 include Gosu
 
@@ -14,26 +11,60 @@ class Game < Gosu::Window
     # Backgrounds / Cursor
     @cursor = Gosu::Image.new(self, 'media/cursor.png')
     @menu = Background.new(self, 'media\\menu.png')
-    @floor = Background.new(self,'media\\floor.png')
     # Text
     @title = Sprite.new(self, 'media\\title.png')
     # Character
-    #@shooter = Sprite.new(self,'media\\shooter.png')
+    @stand_left = Sprite.new(self,'media\\Character/stand_left.png')
+    @stand_right = Sprite.new(self,'media\\Character/stand_right.png')
+    @walk_left = Sprite.new(self,'media\\Character/walk_left.png')
+    @walk_right = Sprite.new(self,'media\\Character/walk_right.png')
+    @shoot_left = Sprite.new(self,'media\\Character/shooting_left.png')
+    @shoot_right = Sprite.new(self,'media\\Character/shooting_right.png')
+    @stand_right.move_to(350, 445)
+    @char = @stand_right
     # Game Detection
-    $scene = 'menu'
-    $game_start = false
-    $score = 0
-    $window = self
-    @char = SceneMap.new
+    @score = 0
     # Game Caption
-    self.caption = "Zombie Shooter - Alpha - 0.0.1"
+    self.caption = "Zombie Shooter - Alpha - 0.1.0"
   end
 
   def update
-    if $game_start == true
-      movement
+    # Start Game
+    if button_down? KbReturn
+      @game_start = true
+        @title.hide
     end
-    @char.update
+    # Updates Sprites
+   if @game_start
+    @stand_left.move_to(@char.x,@char.y)
+    @stand_right.move_to(@char.x,@char.y)
+    @walk_right.move_to(@char.x,@char.y)
+    @walk_left.move_to(@char.x,@char.y)
+    @shoot_left.move_to(@char.x, @char.y)
+    @shoot_right.move_to(@char.x, @char.y)
+    if @dir == :left then
+      @char = @stand_left
+    else
+      @char = @stand_right
+    end
+    # Movement
+    if button_down? KbD
+      @char.adjust_xpos(4)
+      @char = @walk_right
+      @dir = :right
+    elsif button_down? KbA
+      @char.adjust_xpos(-4)
+      @char = @walk_left
+      @dir = :left
+    elsif button_down? KbW
+      @char.adjust_ypos(-10) # Idk why we need this?
+    elsif button_down? KbSpace and @dir == :left
+      @char = @shoot_left
+    elsif button_down? KbSpace and @dir == :right
+      @char = @shoot_right
+    end
+   end
+   #END
   end
 
   def draw
